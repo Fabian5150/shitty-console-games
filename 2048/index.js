a806d6a3625138f34f243e9a82cfdee0f1de939d
Fabian5150 <fabian.strauch.04@web.de>
2024-05-22 22:29:15 +0200

    first lil draft

--- 2048/index.js
+++ 2048/index.js
@@ -5,9 +5,51 @@ const readline = require('readline');
 // eslint-disable-next-line no-undef
 const console = require('console');
 
+class Game {
+    constructor(size) {
+        this.size = size;
+        this.row = new Array(size).fill(0);
+    }
+
+    fillRowRandom() {
+        const numbers = [0, 2, 4, 8, 16];
+        this.row = this.row.map(() => numbers[Math.floor(Math.random() * numbers.length)]);
+    }
+
+    mergedRow() {
+        const row = this.row.filter(n => n !== 0);
+
+        const mergedRow = [];
+
+        for (let current = row.length; current > 0;) {
+            let next = current - 1;
+
+            if (row[next] === row[current]) {
+                // Add the merged numbers to the new row and continue after them
+                mergedRow.push(row[current] * 2);
+                current = next - 1;
+            } else {
+                // Push only the current number to the new row and continue at the next
+                mergedRow.push(row[current]);
+                current = next;
+            }
+        }
+
+        //while (mergedRow.length < this.row.length) mergedRow.unshift(0);
+
+        return mergedRow;
+    }
+}
+
+// Take & convert size argument
+const game = new Game((isNaN(process.argv[2]) || process.argv[2] < 3) ? 4 : Number(process.argv[2]));
+
+game.fillRowRandom();
+console.log(game.row);
+console.log(game.mergedRow());
 
 // Check for key press
-readline.emitKeypressEvents(process.stdin);
+/* readline.emitKeypressEvents(process.stdin);
 
 if (process.stdin.isTTY) process.stdin.setRawMode(true);
 
@@ -35,4 +77,4 @@ process.stdin.on('keypress', (chunk, key) => {
         case 'q':
             process.exit();
     }
-});
+}); */
